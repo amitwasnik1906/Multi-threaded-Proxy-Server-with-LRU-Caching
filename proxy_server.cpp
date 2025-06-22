d78b41ca12b23f84f6d0165a68823624e48028c2
Amit Wasnik <amitwasnik1906@gmail.com>
2025-06-22 13:08:45 +0530

    added CacheElement node and

--- proxy_server.cpp
+++ proxy_server.cpp
@@ -12,6 +12,10 @@
 #include <unistd.h>     // Unix system calls
 #include <fcntl.h>      // File control operations
 #include <sys/wait.h>   // Waiting for child processes
+#include <memory>
+#include <thread>
+#include <mutex>
+#include <semaphore.h>
 #include <errno.h>
 
 using namespace std;
@@ -21,16 +25,51 @@ constexpr int MAX_CLIENTS = 400;                 // max number of client request
 constexpr int MAX_SIZE = 200 * (1 << 20);        // size of the cache (200 MB)
 constexpr int MAX_ELEMENT_SIZE = 10 * (1 << 20); // max size of an element in cache (10 MB)
 
+class CacheElement
+{
+public:
+    string data;                                             // data stores response
+    string url;                                              // url stores the request
+    chrono::time_point<chrono::system_clock> lru_time_track; // lru_time_track stores the latest time the element is accessed
+    shared_ptr<CacheElement> next;                           // pointer to next element
+
+    CacheElement(const string &response_data, const string &request_url)
+        : data(response_data), url(request_url), lru_time_track(chrono::system_clock::now()), next(nullptr) {}
+};
+
 class ProxyServer
 {
 private:
-    int port_number = 8080; // Default Port
-    int proxy_socketId;     // socket descriptor of proxy server
+    int port_number = 8080;        // Default Port
+    int proxy_socketId;            // socket descriptor of proxy server
+    vector<thread> client_threads; // vector to store client threads
+    sem_t semaphore;               // semaphore for limiting concurrent clients
+    mutex cache_lock;              // mutex for cache synchronization
+    shared_ptr<CacheElement> head; // pointer to the cache
+    size_t cache_size = 0;         // cache_size denotes the current size of the cache
 
 public:
-    ProxyServer(int port = 8080) : port_number(port) {}
+    ProxyServer(int port = 8080) : port_number(port), head(nullptr)
+    {
+        sem_init(&semaphore, 0, MAX_CLIENTS);
+    }
 
-    ~ProxyServer() {}
+    ~ProxyServer()
+    {
+        if (proxy_socketId >= 0)
+        {
+            close(proxy_socketId);
+        }
+        // Wait for all threads to complete
+        for (auto &thread : client_threads)
+        {
+            if (thread.joinable())
+            {
+                thread.join();
+            }
+        }
+        sem_destroy(&semaphore);
+    }
 
     void start()
     {
@@ -45,8 +84,7 @@ public:
         }
 
         int reuse = 1;
-        if (setsockopt(proxy_socketId, SOL_SOCKET, SO_REUSEADDR,
-                       reinterpret_cast<const char *>(&reuse), sizeof(reuse)) < 0)
+        if (setsockopt(proxy_socketId, SOL_SOCKET, SO_REUSEADDR, reinterpret_cast<const char *>(&reuse), sizeof(reuse)) < 0)
         {
             perror("setsockopt(SO_REUSEADDR) failed\n");
         }
@@ -58,8 +96,7 @@ public:
         server_addr.sin_addr.s_addr = INADDR_ANY;
 
         // Bind socket
-        if (bind(proxy_socketId, reinterpret_cast<struct sockaddr *>(&server_addr),
-                 sizeof(server_addr)) < 0)
+        if (bind(proxy_socketId, reinterpret_cast<struct sockaddr *>(&server_addr), sizeof(server_addr)) < 0)
         {
             perror("Port is not free\n");
             exit(1);
@@ -82,9 +119,7 @@ public:
             struct sockaddr_in client_addr;
             socklen_t client_len = sizeof(client_addr);
 
-            int client_socketId = accept(proxy_socketId,
-                                         reinterpret_cast<struct sockaddr *>(&client_addr),
-                                         &client_len);
+            int client_socketId = accept(proxy_socketId, reinterpret_cast<struct sockaddr *>(&client_addr), &client_len);
 
             if (client_socketId < 0)
             {
@@ -95,15 +130,12 @@ public:
             // Get client info
             char client_ip[INET_ADDRSTRLEN];
             inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, INET_ADDRSTRLEN);
-            cout << "Client connected from " << client_ip
-                 << ":" << ntohs(client_addr.sin_port) << endl;
-
+            cout << "Client connected from " << client_ip << ":" << ntohs(client_addr.sin_port) << endl;
         }
     }
-
 };
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
     int port = 8080;
 
